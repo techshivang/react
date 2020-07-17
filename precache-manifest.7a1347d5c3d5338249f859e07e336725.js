d92fb7f557df1f393ad602c2ad2e6d8f2e770120
shivang saxena <1616510104@kit.ac.in>
2020-07-17 14:06:55 +0530

    Updates

---
+++ precache-manifest.7a1347d5c3d5338249f859e07e336725.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "227e3836050e64ce978152c58ded7bfd",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "11286fc9791489048bf3",
+    "url": "/react/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "4c78d31c05030a1a6992",
+    "url": "/react/static/css/main.3ced7579.chunk.css"
+  },
+  {
+    "revision": "11286fc9791489048bf3",
+    "url": "/react/static/js/2.8ce4062a.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/react/static/js/2.8ce4062a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c78d31c05030a1a6992",
+    "url": "/react/static/js/main.bafe0be5.chunk.js"
+  },
+  {
+    "revision": "5023b95f3b6bda9f275e",
+    "url": "/react/static/js/runtime-main.b61ebe60.js"
+  },
+  {
+    "revision": "706e94d6ab2d6038a387d6cf2f8d9ef9",
+    "url": "/react/static/media/Machine.706e94d6.jpg"
+  },
+  {
+    "revision": "c71ec212000d05fde61c994d368aaee5",
+    "url": "/react/static/media/office.c71ec212.jpg"
+  },
+  {
+    "revision": "33f3fd3371543dd228bc74689f8c6e1d",
+    "url": "/react/static/media/rocket.33f3fd33.jpg"
+  }
+]);
